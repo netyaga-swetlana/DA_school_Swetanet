874c608e6b03ba941d064e7d1c05d616978fe322
netyaga-swetlana <81932267+netyaga-swetlana@users.noreply.github.com>
2021-05-20 23:50:18 +0300

    Add files via upload

--- lesson_6_homework_Netyaga.sql
+++ lesson_6_homework_Netyaga.sql
@@ -5,6 +5,17 @@
 -- SQL: Создайте таблицу с синтетическими данными (10000 строк, 3 колонки, все типы int) и заполните ее случайными данными от 0 до 1 000 000. Проведите EXPLAIN операции и сравните базовые операции.
 CREATE TABLE test_random_table (col1 int, col2 int, col3 int);
 
+----ВАРИАНТ 1
+FOR loop_counter IN 1 .. 10000
+loop
+   INSERT INTO test_random_table (col1, col2, col3) VALUES ((select floor (Random()*1000000)+1), (select floor (Random()*1000000)+1), (select floor (Random()*1000000)+1))
+end loop;
+
+EXPLAIN SELECT * FROM TEST_RANDOM_TABLE
+EXPLAIN SELECT * FROM test_random_table  WHERE col1 < 888888 AND col2 >222222
+EXPLAIN SELECT col3 FROM test_random_table  WHERE col1 < 888888 AND col2 >222222
+
+---ВАРИАНТ 2т-прекрасно отработал на MS SQL
 DECLARE @var1 int, @var2 int, @var3 int; 
 DECLARE @TestCount int; set @TestCount= 10000; 
 
@@ -16,10 +27,18 @@ begin
    INSERT INTO test_random_table (col1, col2, col3) VALUES (@var1, @var2, @var3);
    SET @TestCount -= 1;
 end 
-
+--------
 EXPLAIN SELECT * FROM TEST_RANDOM_TABLE
 EXPLAIN SELECT * FROM test_random_table  WHERE col1 < 888888 AND col2 >222222
 EXPLAIN SELECT col3 FROM test_random_table  WHERE col1 < 888888 AND col2 >222222
+
+--task2 (lesson6, дополнительно)
+-- GCP (Google Cloud Platform): Через GCP загрузите данные csv в базу PSQL по личным реквизитам (используя только bash и интерфейс bash) 
+
+
+
+
+
 
 --task2 (lesson6, дополнительно)
 -- GCP (Google Cloud Platform): Через GCP загрузите данные csv в базу PSQL по личным реквизитам (используя только bash и интерфейс bash) 
